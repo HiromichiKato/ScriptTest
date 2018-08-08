568e96037faa1eb84707bad72038e6e6b4d8039c
HiromichiKato <cp3xzayn@icloud.com>
2018-08-08 17:50:56 +0900

    ScriptTestプロジェクト　２回目のコミット
    レビューで指摘された箇所を訂正しました。

--- Assets/ScriptTest.cs
+++ Assets/ScriptTest.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptTest : MonoBehaviour {
+public class ScriptTest : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        int[] array = new int[5];          array[0] = 20;         array[1] = 25;         array[2] = 48;         array[3] = 82;         array[4] = 90;          for (int i = 4; i > -1; i--)
-        {             Debug.Log(array[i]);         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 20;
+        array[1] = 25;
+        array[2] = 48;
+        array[3] = 82;
+        array[4] = 90;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = 4; i > -1; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
